e09da489ce312db513155abb833cbf5935296ec4
Zamir <argashokovzamir@gmail.com>
2021-01-25 22:21:07 +0100

    Initial Commit

---
+++ UILessonsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILessonsApp
+//
+//  Created by Zamir Argashokov on 25.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
